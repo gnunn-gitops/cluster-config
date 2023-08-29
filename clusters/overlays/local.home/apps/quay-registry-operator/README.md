92ebe1ce4c2972e9765c4f12e4d81f7731903d5c
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-08-29 09:39:52 -0700

    Remove apps that won't be on home cluster

--- clusters/overlays/local.home/apps/quay-registry-operator/README.md
+++
@@ -1,1 +0,0 @@
-Note you need to manually generate the config bundle secret in the generate folder, at some point I'll fully gitops this process.
--- clusters/overlays/local.home/apps/quay-registry-operator/generate/generate-certs.sh
+++
@@ -1,7 +0,0 @@
-# Using Route53 with DNS challenge, source AWS creds (not in git repo but just sets access and secret key as env variables)
-. aws-creds
-mkdir -p qnap-tls
-acme.sh --force --issue --dns dns_aws --cert-file "./qnap-tls/cert.pem" --key-file "./qnap-tls/key.pem" --fullchain-file "./qnap-tls/fullchain.pem" --ca-file "./qnap-tls/ca.cer" -d registry.apps.home.ocplab.com
-
-# Convert CA to crt
-openssl x509 -inform PEM -in ./qnap-tls/ca.cer -out ./qnap-tls/letsencrypt_ca.crt
